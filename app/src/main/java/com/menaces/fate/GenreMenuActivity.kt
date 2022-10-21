aa21626cff043b6a6383de803f48875e0d19139f
katrina28tobias <katrina28tobias@utexas.edu>
2022-10-20 19:51:45 -0500

    worked on homescreen and genre menu

---
+++ app/src/main/java/com/menaces/fate/GenreMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.menaces.fate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenreMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_genre_menu)
+    }
+}
--- app/src/main/java/com/menaces/fate/MainActivity.kt
+++ app/src/main/java/com/menaces/fate/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.menaces.fate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var genreIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton : Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            launchGenreMenu()
+        }
     }
+
+    private fun launchGenreMenu() {
+        genreIntent = Intent(this, GenreMenuActivity::class.java)
+        startActivity(genreIntent)
+    }
+
+
 }
