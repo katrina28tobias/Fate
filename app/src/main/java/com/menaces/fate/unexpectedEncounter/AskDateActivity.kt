6ebbfb41ca5a633ccf81d66a1a78f3f459ee1547
megan.noronha <megan.noronha@utexas.edu>
2022-10-27 20:11:13 -0500

    Added ask date activity

---
+++ app/src/main/java/com/menaces/fate/unexpectedEncounter/AskDateActivity.kt
@@ -0,0 +1,12 @@
+package com.menaces.fate.unexpectedEncounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.menaces.fate.R
+
+class AskDateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_date)
+    }
+}
--- app/src/main/java/com/menaces/fate/unexpectedEncounter/NameErrorActivity.kt
+++ app/src/main/java/com/menaces/fate/unexpectedEncounter/NameErrorActivity.kt
@@ -1,12 +1,25 @@
 package com.menaces.fate.unexpectedEncounter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.menaces.fate.R
 
 class NameErrorActivity : AppCompatActivity() {
+    private lateinit var sIntent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name_error)
+
+        val leftButton : Button = findViewById(R.id.name_error_left_button)
+        leftButton.setOnClickListener {
+            launchAskDate()
+        }
+    }
+
+    private fun launchAskDate() {
+        sIntent = Intent(this, AskDateActivity::class.java)
+        startActivity(sIntent)
     }
 }
